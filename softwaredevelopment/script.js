a5379d118a441b94cb482beba4d7353e7500d468
Abhiraj Mhetre <55054832+AbhirajMhetre@users.noreply.github.com>
2020-11-03 08:50:47 +0530

    new services added

---
+++ softwaredevelopment/script.js
@@ -0,0 +1,59 @@
+
+// **********************************//
+
+//horizontal navbar
+$(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+            $("nav ul").toggleClass("showing");
+      });
+    });
+    
+    // Scrolling Effect
+    
+    $(window).on("scroll", function() {
+      if($(window).scrollTop()) {
+            $('nav').addClass('black');
+      }
+    
+      else {
+            $('nav').removeClass('black');
+      }
+    })
+    
+    //horizontal navbar end
+    
+    
+    
+    
+    
+    //vertical navbar
+    $(document).ready(function () {
+      var trigger = $('.hamburger'),
+          overlay = $('.overlay'),
+         isClosed = false;
+    
+        trigger.click(function () {
+          hamburger_cross();      
+        });
+    
+        function hamburger_cross() {
+    
+          if (isClosed == true) {          
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+          } else {   
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+          }
+      }
+      
+      $('[data-toggle="offcanvas"]').click(function () {
+            $('#wrapper').toggleClass('toggled');
+      });  
+    });
+    
+    //vertical navbar end
